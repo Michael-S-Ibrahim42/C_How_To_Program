4b363a74cdedc7f5cdade1f1c734173e59d4f0b2
Michael Ibrahim <michael.ibrahim042@gmail.com>
2021-12-26 07:54:05 +0200

    Chapter4_prob17

--- Triangle_Printing_Program.c
+++ Triangle_Printing_Program.c
@@ -11,12 +11,44 @@
 int main(int args, char ** argv){
 	int Counter1, Counter2;
 	int Rows = 10, Columns = 10;
-	
+	/* First Pattern */
 	for(Counter1 = 0; Counter1 < Rows; Counter1++){
-		for(Counter2 = 0; Counter2 < Columns; Counter2){
+		for(Counter2 = 0; Counter2 < (Counter1+1); Counter2++){
 			printf("*");
 		}//for
+		printf("\n");
 	}//for
+	printf("\n");
+	/* Printing second pattern */
+	for(Counter1 = 0; Counter1 < Rows; Counter1++){
+		for(Counter2 = (Columns-1-Counter1); Counter2 >= 0; Counter2--){
+			printf("*");
+		}//for
+		printf("\n");
+	}//for
+	printf("\n");
+	/* Printing third pattern */
+	for(Counter1 = 0; Counter1 < Rows; Counter1++){
+		for(Counter2 = 0; Counter2 < Counter1; Counter2++){
+			printf(" ");
+		}//for
+		for(Counter2 = (Columns-1-Counter1); Counter2 >= 0; Counter2--){
+			printf("*");
+		}//for
+		printf("\n");
+	}//for
+	printf("\n");
+	/* printing fourth pattern */
+	for(Counter1 = 0; Counter1 < Rows; Counter1++){
+		for(Counter2 = Counter1; Counter2 < Columns-1; Counter2++){
+			printf(" ");
+		}//for
+		for(Counter2 = Counter1; Counter2 >= 0; Counter2--){
+			printf("*");
+		}//for
+		printf("\n");
+	}//for	
+	printf("\n");
 
 	return(0);//Indicate normal termination of a program
 }//main
