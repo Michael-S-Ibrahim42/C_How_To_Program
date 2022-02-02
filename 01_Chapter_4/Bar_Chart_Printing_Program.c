7e9fe9ba717caa847184559611bc10fac5adbb99
Michael Ibrahim <michael.ibrahim042@gmail.com>
2022-02-02 10:46:02 +0200

    Add bar chart program

--- 01_Chapter_4/Bar_Chart_Printing_Program.c
+++ 01_Chapter_4/Bar_Chart_Printing_Program.c
@@ -1,25 +1,30 @@
-/*
-* Name         :           Michael S. Ibrahim
-* Date         :           December 27, 2021
-* Version      :           V01
-* SWC          :	 	   Bar Chart Printing Program
-*/
+/* **************************************************************************************
+**       Author      :	Michael S. Ibrahim
+**       Date        :	February 02, 2022
+**       Version     :	V01
+**       SWC         :	Bar_Chart_Printing
+**       Description :	C How To Program Ref Chapter4 problem
+** **************************************************************************************/
 /* STD Headers */
 #include <stdio.h>
+#define SIZE          5
 
-enum demo {
-    FOO,
-    BAR,
-    BAZ,
-    BOO,
-    GOTCHA
-};
-
-int main() {
-    enum demo value;
-    value = FOO;
-    printf("Normal value: %d\n", value);
-
-    value = 10;
-    printf("Out of range: %d\n", value);
-};
+/* main Fn */
+int main(int args, char ** argv){
+  int Data[SIZE];
+  int Iterator1, Iterator2;
+  
+  for(Iterator1 = 0; Iterator1 < SIZE; Iterator1++){
+    printf("Enter Data of the index %d: ", Iterator1+1);
+    scanf("%d", &Data[Iterator1]);
+  }//for  
+  for(Iterator1 = 0; Iterator1 < SIZE; Iterator1++){
+    for(Iterator2 = 0; Iterator2 < Data[Iterator1]; Iterator2++){
+      printf("*");
+    }//for
+    printf("\n");
+  }//for
+  
+  
+  return(0);//Indicate normal main termination
+}//main
