dd31c9db664056f2ee2a5aca1f2b1f1fba84c8f0
Michael Ibrahim <michael.ibrahim042@gmail.com>
2022-02-14 19:46:24 +0200

    Make some directories moves

---
+++ 01_Chapter_4/Decimal_Binary_Octal_Hexadecimal/Numbering_Systems.c
@@ -0,0 +1,47 @@
+/* **************************************************************************************
+**       Author      :	Michael S. Ibrahim
+**       Date        :	February 13, 2022
+**       Version     :	V01
+**       SWC         :	Numbering_Systems
+**       Description :	Chapter4 problem 2.25
+** **************************************************************************************/
+/* STD Headers */
+#include <stdio.h>
+
+/* main Fn */
+int main(int args, char ** argv)
+{
+  int Iterator, TempVar;
+  long int Binary, Octal, Hexadecimal;
+  
+  for(Iterator = 1; Iterator < 257; Iterator++)
+  {
+    TempVar = Iterator;
+    /* Binary Conversion */
+    while(TempVar)
+    {
+      
+    }/* while */
+    /* Octal Conversion */
+    while(TempVar)
+    {
+      
+    }/* while */
+    /* Hexadecimal Conversion */
+    while(TempVar)
+    {
+      
+    }/* while */
+    
+    
+  }/* for */
+  
+  
+  return(0);/* indicate normal program termination */
+}/* main */
+
+
+Loop over numbers from 1 to 256
+  loop to convert to binary
+  loop to convert to octal
+  loop to convert to hexa
